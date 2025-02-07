10c77cb3fece1bcf0ae64adddc6272a54b81dde7
bart-maykin <bart.burgman@maykinmedia.nl>
2025-02-07 17:08:42 +0100

    :memo: [#10] added help_text to certain serializer fields

--- src/woo_search/search_index/api/serializers/publications.py
+++ src/woo_search/search_index/api/serializers/publications.py
@@ -1,16 +1,31 @@
+from django.utils.translation import gettext_lazy as _
+
 from rest_framework import serializers
 
 
 class DocumentSerializer(serializers.Serializer):
     uuid = serializers.UUIDField()
-    publicatie = serializers.UUIDField()
+    publicatie = serializers.UUIDField(
+        help_text=_("The unique identifier of the publication."),
+    )
     publisher = serializers.UUIDField()
-    identifier = serializers.CharField(max_length=255, required=False, allow_blank=True)
+    identifier = serializers.CharField(
+        help_text=_("De (primaire) unieke identificatie."),
+        max_length=255,
+        required=False,
+        allow_blank=True,
+    )
     officiele_titel = serializers.CharField(max_length=255)
     verkorte_titel = serializers.CharField(
         max_length=255, required=False, allow_blank=True
     )
     omschrijving = serializers.CharField(required=False)
-    creatiedatum = serializers.DateField()
+    creatiedatum = serializers.DateField(
+        help_text=_(
+            "De datum waarop het document ontstaan is. "
+            "Niet te verwarren met de registratiedatum - "
+            "de creatiedatum valt typisch voor de registratiedatum."
+        )
+    )
     registratiedatum = serializers.DateTimeField()
     laatst_gewijzigd_datum = serializers.DateTimeField()
