e601acbf3ce1d1e45be75e509f54dfded1e88110
bart-maykin <bart.burgman@maykinmedia.nl>
2025-02-07 16:58:55 +0100

    :art: [#10] made certain fields required and changed the id to the uuid.

--- src/woo_search/search_index/documents/publications.py
+++ src/woo_search/search_index/documents/publications.py
@@ -2,16 +2,16 @@ from elasticsearch_dsl import Date, Document as ES_Document, Text
 
 
 class Document(ES_Document):
-    uuid = Text()
-    publicatie = Text()
-    publisher = Text()
+    uuid = Text(required=True)
+    publicatie = Text(required=True)
+    publisher = Text(required=True)
     identifier = Text()
-    officiele_titel = Text()
+    officiele_titel = Text(required=True)
     verkorte_titel = Text()
     omschrijving = Text()
-    creatiedatum = Date()
-    registratiedatum = Date()
-    laatst_gewijzigd_datum = Date()
+    creatiedatum = Date(required=True)
+    registratiedatum = Date(required=True)
+    laatst_gewijzigd_datum = Date(required=True)
 
     class Index:
         name = "document"
