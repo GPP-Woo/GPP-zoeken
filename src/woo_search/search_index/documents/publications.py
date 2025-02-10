8b39624a1bfa97d1e6b8ebfafc72a1f13b2893fe
Sergei Maertens <sergei@maykinmedia.nl>
2025-02-10 22:24:05 +0100

    :label: [#10] Improve on type hints for elasticsearch-dsl
    The library documentation includes some hints on how to work
    with modern python type hints/annotations, see:
    https://elasticsearch-dsl.readthedocs.io/en/latest/persistence.html\#python-type-hints
    
    This gives better control over the retrieved data and
    related type information, helping us assert that the correct
    data type was used in the elastic search mappings.

--- src/woo_search/search_index/documents/publications.py
+++ src/woo_search/search_index/documents/publications.py
@@ -1,17 +1,26 @@
-from elasticsearch_dsl import Date, Document as ES_Document, Text
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from elasticsearch_dsl import Date, Document as ES_Document, M, Text, mapped_field
 
 
 class Document(ES_Document):
-    uuid = Text(required=True)
-    publicatie = Text(required=True)
-    publisher = Text(required=True)
-    identifier = Text()
-    officiele_titel = Text(required=True)
-    verkorte_titel = Text()
-    omschrijving = Text()
-    creatiedatum = Date(required=True)
-    registratiedatum = Date(required=True)
-    laatst_gewijzigd_datum = Date(required=True)
+    # See https://elasticsearch-dsl.readthedocs.io/en/latest/persistence.html#python-type-hints
+    # for typing support.
+    uuid: M[str] = mapped_field(Text(required=True))
+    publicatie: M[str] = mapped_field(Text(required=True))
+    publisher: M[str] = mapped_field(Text(required=True))
+    identifier: M[str | None] = mapped_field(Text())
+    officiele_titel: M[str] = mapped_field(Text(required=True))
+    verkorte_titel: M[str | None] = mapped_field(Text())
+    omschrijving: M[str | None] = mapped_field(Text())
+    creatiedatum: M[datetime] = mapped_field(Date(required=True))
+    registratiedatum: M[datetime] = mapped_field(Date(required=True))
+    laatst_gewijzigd_datum: M[datetime] = mapped_field(Date(required=True))
+
+    if TYPE_CHECKING:
+        # help the type checkers a little bit
+        _id: str
 
     class Index:
         name = "document"
--- src/woo_search/search_index/tasks/publications.py
+++ src/woo_search/search_index/tasks/publications.py
@@ -6,7 +6,6 @@ from ..documents import Document
 
 @app.task()
 def save_document(data: dict):
-    document = Document(
-        meta={"id": data["uuid"]}, **data  # pyright: ignore reportCallIssue
-    )
-    document.save(using=get_client())
+    document = Document(_id=data["uuid"], **data)
+    with get_client() as client:
+        document.save(using=client)
