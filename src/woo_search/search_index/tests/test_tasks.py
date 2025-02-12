e718c8d449b7ea0835c371a07db891c8e5d5dedd
bart-maykin <bart.burgman@maykinmedia.nl>
2025-02-12 17:26:45 +0100

    :white_check_mark: [#10] added test to see if es data of same uuid updates

--- src/woo_search/search_index/tests/test_tasks.py
+++ src/woo_search/search_index/tests/test_tasks.py
@@ -66,3 +66,57 @@ class DocumentTaskTest(VCRMixin, ElasticSearchTestCase):
             doc.laatst_gewijzigd_datum,
             datetime(2026, 1, 5, 12, 0, 0, tzinfo=timezone.utc),
         )
+
+        with self.subTest("re-indexing data with same UUID updates values"):
+            index_document(
+                uuid=document_uuid,
+                publicatie="CHANGED",
+                publisher={
+                    "uuid": "CHANGED",
+                    "naam": "Amsterdam",
+                },
+                identifier="https://www.example.com/999",
+                officiele_titel="CHANGED TITLE",
+                verkorte_titel="CHANGED",
+                omschrijving="CHANGED OMSCHRIJVING",
+                creatiedatum=date(2030, 1, 1),
+                registratiedatum=datetime(2030, 1, 5, 12, 0, 0, tzinfo=timezone.utc),
+                laatst_gewijzigd_datum=datetime(
+                    2030, 1, 5, 12, 0, 0, tzinfo=timezone.utc
+                ),
+            )
+
+            with get_client() as client:
+                updated_doc = Document.get(
+                    using=client,
+                    id=document_uuid,
+                )
+
+            assert isinstance(updated_doc, Document), "Expected doc to be indexed"
+            # Assert the provided data is indexed properly.
+            self.assertEqual(updated_doc.uuid, "0095704d-4216-4de3-83d2-20dba551b0dc")
+            self.assertEqual(updated_doc.publicatie, "CHANGED")
+            self.assertEqual(
+                updated_doc.publisher,
+                {
+                    "uuid": "CHANGED",
+                    "naam": "Amsterdam",
+                },
+            )
+            self.assertEqual(updated_doc.identifier, "https://www.example.com/999")
+            self.assertEqual(updated_doc.officiele_titel, "CHANGED TITLE")
+            self.assertEqual(updated_doc.verkorte_titel, "CHANGED")
+            self.assertEqual(updated_doc.omschrijving, "CHANGED OMSCHRIJVING")
+            # date -> converted to naive datetime
+            self.assertEqual(
+                updated_doc.creatiedatum,
+                datetime(2030, 1, 1, 0, 0, 0, tzinfo=TIMEZONE_AMS),
+            )
+            self.assertEqual(
+                updated_doc.registratiedatum,
+                datetime(2030, 1, 5, 12, 0, 0, tzinfo=timezone.utc),
+            )
+            self.assertEqual(
+                updated_doc.laatst_gewijzigd_datum,
+                datetime(2030, 1, 5, 12, 0, 0, tzinfo=timezone.utc),
+            )
