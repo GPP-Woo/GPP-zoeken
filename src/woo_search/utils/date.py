c20d5824eda15123458bf7d926b341c21af15eef
bart-maykin <bart.burgman@maykinmedia.nl>
2025-02-19 11:31:36 +0100

    :art: changed creatiedatum field in ES to an actual date field instead of datetime field

--- src/woo_search/utils/date.py
+++
@@ -1,9 +0,0 @@
-from datetime import date, datetime
-from zoneinfo import ZoneInfo
-
-TIMEZONE_AMS = ZoneInfo("Europe/Amsterdam")
-
-
-def date_to_datetime(value: date, timezone: ZoneInfo = TIMEZONE_AMS) -> datetime:
-    """return the date as datetime of specific timezone, default timezone is Europe/Amsterdam"""
-    return datetime.combine(value, datetime.min.time(), tzinfo=timezone)
