c514faa1863e0888da553506e93d0f60d698b538
bart-maykin <bart.burgman@maykinmedia.nl>
2025-02-25 08:27:21 +0100

    :white_check_mark: [#10, #11] Add assertion for delete endpoint permissions

--- src/woo_search/api/tests/mixin.py
+++ src/woo_search/api/tests/mixin.py
@@ -48,6 +48,49 @@ class APIKeyUnAuthorizedMixin:
                 response.status_code, status.HTTP_403_FORBIDDEN
             )
 
+    def assertWrongApiKeyProhibitsDeleteEndpointAccess(self, url):
+        no_permission_token = TokenAuthFactory.create(permissions=[]).token
+        read_token = TokenAuthFactory.create(read_permission=True).token
+
+        with self.subTest(  # pyright: ignore[reportAttributeAccessIssue]
+            "no token given"
+        ):
+            response = self.client.delete(url)
+            self.assertEqual(  # pyright: ignore[reportAttributeAccessIssue]
+                response.status_code, status.HTTP_401_UNAUTHORIZED
+            )
+
+        with self.subTest(  # pyright: ignore[reportAttributeAccessIssue]
+            "none existing token"
+        ):
+            response = self.client.delete(
+                url, headers={"Authorization": "Token broken"}
+            )
+            self.assertEqual(  # pyright: ignore[reportAttributeAccessIssue]
+                response.status_code, status.HTTP_401_UNAUTHORIZED
+            )
+
+        with self.subTest(  # pyright: ignore[reportAttributeAccessIssue]
+            "token with no permission"
+        ):
+            response = self.client.delete(
+                url,
+                headers={"Authorization": f"Token {no_permission_token}"},
+            )
+            self.assertEqual(  # pyright: ignore[reportAttributeAccessIssue]
+                response.status_code, status.HTTP_403_FORBIDDEN
+            )
+
+        with self.subTest(  # pyright: ignore[reportAttributeAccessIssue]
+            "token with wrong permission"
+        ):
+            response = self.client.delete(
+                url, headers={"Authorization": f"Token {read_token}"}
+            )
+            self.assertEqual(  # pyright: ignore[reportAttributeAccessIssue]
+                response.status_code, status.HTTP_403_FORBIDDEN
+            )
+
 
 class TokenAuthMixin:
     client: APIClient
