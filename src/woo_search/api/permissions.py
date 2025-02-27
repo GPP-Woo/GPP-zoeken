5cc9218a747125c99bbcff0f8f9b09929de3510c
bart-maykin <bart.burgman@maykinmedia.nl>
2025-02-27 12:57:52 +0100

    :sparkles: [#48] added  permission class.

--- src/woo_search/api/permissions.py
+++ src/woo_search/api/permissions.py
@@ -28,3 +28,24 @@ class TokenAuthPermission(BasePermission):
             return True
 
         return False
+
+
+class TokenAuthReadPermission(BasePermission):
+    """
+    Simplified version of the `TokenAuthPermission` permission class.
+    Instead of checking for `read` or `write` based on the request
+    this permission class only checks if the user has read permissions no matter the request.
+    """
+
+    def has_permission(self, request, view) -> bool:
+        token = request.auth
+
+        if not isinstance(token, Application):
+            return False
+
+        assert isinstance(token.permissions, list)
+
+        if PermissionOptions.read in token.permissions:
+            return True
+
+        return False
