fd45844c8a3190a3797872a20685832cbf6b78b9
Sergei Maertens <sergei@maykinmedia.nl>
2025-05-28 13:53:40 +0200

    :fire: Remove checks from codebase
    The same checks have been provided in maykin-common
    already, so we don't need to track them in the project
    itself.
    
    Additionally, the ruff rule DJ006 obsoletes the need
    for the model form system check entirely.

--- src/woo_search/search_index/utils.py
+++ src/woo_search/search_index/utils.py
@@ -2,7 +2,11 @@ from collections.abc import Iterator
 
 from elasticsearch_dsl import Document
 
-from woo_search.utils.checks import get_subclasses
+
+def get_subclasses(cls: type):
+    for subclass in cls.__subclasses__():
+        yield from get_subclasses(subclass)
+        yield subclass
 
 
 def get_index_document_types() -> Iterator[type[Document]]:
